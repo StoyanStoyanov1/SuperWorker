7e641390fce3d36f4c2ba11b688130817378ad48
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-04-11 19:14:02 +0200

    Add CORS support and enhance product schema with additional fields

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,9 +1,53 @@
-import mongoose from "mongoose";
+import mongoose, {Mongoose} from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number,
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    richDescription: {
+        type: String,
+        default: "",
+    },
+    image: {
+        type: String,
+        default: "",
+    },
+    images: [{
+        type: String,
+        default: "",
+    }],
+    price: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: Mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+    },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 export default mongoose.model('Product', productSchema);
